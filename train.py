7db7102768452327bd45d0964b9c5a2878c1ed81
FelixChristian011226 <1930552052@qq.com>
2024-10-27 21:10:46 +0800

    feat: improved the train process. added log for 10 epochs with new method.

--- train.py
+++ train.py
@@ -9,11 +9,15 @@ import torchvision
 from torchvision.models.detection import fasterrcnn_resnet50_fpn
 
 from torch.utils.tensorboard import SummaryWriter
+from torch.cuda.amp import GradScaler, autocast
 
 
 def main():
 
-    writer = SummaryWriter(log_dir='log_dir/old_method')
+    num_epochs = 10
+
+    writer = SummaryWriter(log_dir='log_dir/new_method')
+    scaler = GradScaler()
     
     # 构造数据加载器
     # dataset = CityscapesDataset('./data/cityscapes',split="train")
@@ -25,7 +29,8 @@ def main():
     # 过滤后的标签数量（加上背景类）
     num_classes = len(name2label.keys()) + 1  # 加1是因为要包括背景类
     # 加载预训练的 Faster R-CNN 模型
-    model = fasterrcnn_resnet50_fpn(pretrained=True)
+    # model = fasterrcnn_resnet50_fpn(pretrained=True)
+    model = fasterrcnn_resnet50_fpn(pretrained=True, backbone_name="resnet101")
     # 修改分类头
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     model.roi_heads.box_predictor = torchvision.models.detection.faster_rcnn.FastRCNNPredictor(in_features, num_classes)
@@ -36,10 +41,10 @@ def main():
     # 优化器和学习率调度器
     params = [p for p in model.parameters() if p.requires_grad]
     optimizer = optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+    # lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=num_epochs)
 
     # 开始训练
-    num_epochs = 10
     for epoch in range(num_epochs):
         model.train()
         epoch_loss = 0
@@ -47,15 +52,16 @@ def main():
             images = list(image.to(device) for image in images)
             targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
-            # 前向传播和计算损失
-            loss_dict = model(images, targets)
-            losses = sum(loss for loss in loss_dict.values())
-            epoch_loss += losses.item()
-
-            # 反向传播
             optimizer.zero_grad()
-            losses.backward()
-            optimizer.step()
+            with autocast():
+                loss_dict = model(images, targets)
+                losses = sum(loss for loss in loss_dict.values())
+                epoch_loss += losses.item()
+
+            # 用 scaler 进行反向传播
+            scaler.scale(losses).backward()
+            scaler.step(optimizer)
+            scaler.update()
 
         # 记录数据到Tensorboard
         writer.add_scalar('Loss/train', epoch_loss, epoch)
@@ -66,7 +72,7 @@ def main():
     print(f"Epoch {epoch + 1}/{num_epochs}, Loss: {epoch_loss}")
 
     # 训练结束后保存模型
-    torch.save(model.state_dict(), 'fasterrcnn_cityscapes.pth')
+    torch.save(model.state_dict(), 'fasterrcnn_cityscapes_new.pth')
 
     writer.close()
 
