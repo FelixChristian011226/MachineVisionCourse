fix: deleted unnecessary parameter calculation in evalution.

eval.py
@@ -20,7 +20,7 @@ def evaluate_model(model, dataloader, iou_threshold=0.5):
         results: 包含 mAP、Precision 和 F1 Score 的字典
     """
     model.eval()
-    aps, precisions, f1_scores = [], [], []
+    aps, f1_scores = [], []
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     with torch.no_grad():
@@ -36,14 +36,12 @@ def evaluate_model(model, dataloader, iou_threshold=0.5):
                 
                 # 计算 IoU 并过滤预测框
                 iou_matrix = torchvision.ops.box_iou(pred_boxes, gt_box)
-                tp = (iou_matrix >= iou_threshold).any(dim=1).sum().item()
-                fp = len(pred_boxes) - tp
+                tp = (iou_matrix >= iou_threshold).any(dim=1).sum().item()  # 真正例
+                fp = len(pred_boxes) - tp                                   # 假正例
 
                 # 计算 Precision 和 F1 Score
                 precision = tp / (tp + fp) if (tp + fp) > 0 else 0
                 f1 = 2 * precision / (1 + precision) if precision > 0 else 0
-
-                precisions.append(precision)
                 f1_scores.append(f1)
                 
                 # 计算平均精度（AP）
@@ -52,12 +50,10 @@ def evaluate_model(model, dataloader, iou_threshold=0.5):
 
     # 计算 mAP
     mAP = np.mean(aps)
-    avg_precision = np.mean(precisions)
     avg_f1_score = np.mean(f1_scores)
 
     results = {
         "mAP": mAP,
-        "Precision": avg_precision,
         "F1 Score": avg_f1_score
     }
     return results
@@ -76,7 +72,7 @@ num_classes = len(filterd_label) + 1  # 加1是因为要包括背景类，在pyt
 in_features = model.roi_heads.box_predictor.cls_score.in_features
 model.roi_heads.box_predictor = torchvision.models.detection.faster_rcnn.FastRCNNPredictor(in_features, num_classes)
 model = model.to(device)
-model.load_state_dict(torch.load("./fasterrcnn_cityscapes_old.pth"))
+model.load_state_dict(torch.load("./fasterrcnn_cityscapes_new_200.pth"))
 
 
 